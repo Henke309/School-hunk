197e9eb7a89bfe7884b5ada9846dd959c5f8ab77
Henrik Nguyen <EDUheng1@utb.karlskoga.se>
2020-10-22 21:10:04 +0200

    .

---
+++ Programmering/3an/inl1class_HenrikNguyen.py
@@ -0,0 +1,71 @@
+
+from abc import ABCMeta, abstractmethod
+
+class djur ():
+    idcounter = 100
+
+    def __init__(self, namn, ras, vikt):
+        self.namn = namn
+        self.setdjurid()
+        self.ras = ras
+        self.vikt = vikt
+
+    @property
+    def djurid(self):
+        return self.__djurid
+
+    def setdjurid (self):
+        self.__djurid = djur.idcounter
+        djur.idcounter += 1
+
+    @property
+    def namn(self):
+        return self.__namn.title()
+
+    @namn.setter
+    def namn(self, value):
+        self.__namn = value
+        
+    @property
+    def vikt (self):
+        return self.__vikt
+
+    @vikt.setter 
+    def vikt (self, value):
+        if not value.isdigit():
+            raise SyntaxError("Skriv med siffor mongo...")
+        else:
+            if not value.strip():
+                raise SyntaxError("Ha inga mellanslag...")
+            
+            else:
+                self.__vikt = value
+
+    @property
+    def ras (self):
+        return self.__ras.title()
+
+    @ras.setter
+    def ras (self, value):
+        if value.isalpha():
+            self.__ras = value 
+
+        else:
+            raise SyntaxError("VILKEN RAS HAR EN SIFFRA I SITT NAMN?!?!?!?\nEller special tecken (. - _ osv)")
+
+    #@abstractmethod
+    def info(self):
+        return "Namn " + self.namn +\
+            "\nId-nr: " + str(self.djurid) +\
+                "\nRas: " + self.ras +\
+                    "\nVikt: " + self.vikt
+
+
+class katt (djur):
+    def __init__(self, namn, ras, vikt):
+        djur.__init__(self, namn, ras, vikt)
+
+class hund (djur):
+    def __init__(self, namn, ras, vikt):
+        djur.__init__(self, namn, ras, vikt)
+
---
+++ Programmering/3an/inl1prog_HenrikNguyen.py
@@ -0,0 +1,115 @@
+from inl1class_HenrikNguyen import *
+from abc import ABCMeta, abstractmethod
+
+djurpensionat = []
+while True:      
+    print("*"*50)
+    print("Meny".center(50))
+    print("*"*50)
+    print("1. lägg in ett djur ------------------------------")
+    print("2. ta bort ett djur ------------------------------")
+    print("3. sök efter ett djur ----------------------------")
+    print("4. se alla djur ----------------------------------")
+    print("5. ändringar på djuren ---------------------------")
+    print("0. Avsluta programmet ----------------------------")
+    print("")
+    meny = input("Välj något av alternativen ovan: ")
+
+    if meny == "1":
+        while True:
+            print("")
+            print("*"*50)
+            print("Lägg till ett djur".center(50))
+            print("*"*50)
+            print("")
+            meny2 = input("Ska du lägga till en katt eller en hund?" "\n" "Svara med: 'katt' eller 'hund'" "\n" "svara här: ")
+            
+            
+            if meny2.lower() == "katt":
+                print("Du har valt att lägga in en katt på djurpensionatet")
+                
+                while True:
+                    namn = input("Mata in kattens namn: ").title()
+                    if namn == "":
+                        print("*"*50)
+                        print("Du måste skriva ett namn.")
+                        print("*"*50)
+                    else:
+                        break
+                        
+                while True:
+                    ras = input("Mata in " + namn.title() + "s" + " ras: " )
+                    if ras == "":
+                        print("*"*50)
+                        print("Du måste skriva en ras.")
+                        print("*"*50)
+                    else:
+                        break
+
+                while True:
+                    vikt = input("Ange " + namn.title() + "s" + " vikt: ")
+                    if not vikt.strip().isdigit():
+                        print("*"*50)
+                        print("Inga mellanslag eller bokstäver, endast siffor")
+                        print("*"*50)
+                    else:
+                        break
+
+                djurpensionat.append(katt(namn, ras, vikt))
+                        
+
+            elif meny2.lower() == "hund":
+                print("Du har valt att lägga in en hund på djurpensionatet")
+
+                while True:
+                    namn = input("Mata in hundens namn: ").title()
+                    if namn == "":
+                        print("*"*50)
+                        print("Du måste skriva ett namn.")
+                        print("*"*50)
+                    else:
+                        break
+                        
+                while True:
+                    ras = input("Mata in " + namn.title() + "s" + " ras: " )
+                    if ras == "":
+                        print("*"*50)
+                        print("Du måste skriva en ras.")
+                        print("*"*50)
+                    else:
+                        break
+
+                while True:
+                    vikt = input("Ange " + namn.title() + "s" + " vikt: ")
+                    if not vikt.strip().isdigit():
+                        print("*"*50)
+                        print("Inga mellanslag eller bokstäver, endast siffor")
+                        print("*"*50)
+                    else:
+                        break
+
+                djurpensionat.append(hund(namn, ras, vikt))
+
+
+    elif meny == "2":
+        pass
+
+
+    elif meny == "3":
+        pass
+   
+    
+    elif meny == "4":
+        pass
+      
+
+    elif meny == "5":
+        pass
+
+
+    elif meny == "0":
+        print("Du har valt att avsluta programmet!")
+        quit
+
+    else:
+        print("Det måste vara med en siffra...")
